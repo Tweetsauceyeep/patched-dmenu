545f8be61627973308b128d2d157526c22f2ee25
Tweetsauce <pobusauce12@gmail.com>
2022-11-19 13:23:14 +0800

    added numbers patch

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font:Bold:size=13"
+	"Iosevka Nerd Font:Bold:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ECEFF4", "#2E3440" },
-	[SchemeSel] = { "#eeeeee", "#81A1C1" },
+	[SchemeSel] = { "#eeeeee", "#5E81AC" },
 	[SchemeOut] = { "#000000", "#2E3440" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
