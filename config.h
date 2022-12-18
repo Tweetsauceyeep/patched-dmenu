1b30cf18c459b0c16ae2b922dd61a039ba904ef9
Tweetsauce <pobusauce12@gmail.com>
2022-12-18 17:10:16 +0800

    YEAHHHH BORDER PATCVH WORKINGGG

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static const unsigned int border_width = 3;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Iosevka Nerd Font:Bold:size=11"
